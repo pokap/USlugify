6d8a4b8b9465921a554f8f877bc0cef92dc40da9
Florent Denis <dflorent.pokap@gmail.com>
2014-12-29 16:09:25 +0100

    Update interface

--- src/USlugifyInterface.php
+++ src/USlugifyInterface.php
@@ -13,8 +13,10 @@ interface USlugifyInterface
      * Return a slug of a string.
      *
      * @param string $text
+     * @param string $separator Default -
+     * @param bool   $lower     Default FALSE
      *
      * @return string
      */
-    public function slugify($text);
+    public function slugify($text, $separator = '-', $lower = false);
 }
